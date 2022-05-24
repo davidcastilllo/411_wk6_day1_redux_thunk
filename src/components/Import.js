9719e3db8e4545ee688463fb19480ffdfdc59e16
David Castillo <seth.castillo4@gmail.com>
2022-05-23 21:14:34 -0500

    made progress

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from "@material-ui/core"
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+     <>
+        <Button variant="contained" color="primary" onClick={props.fetchMakes}>Import</Button>
+        <Table>
+         <TableHead>
+          <TableRow>Id</TableRow>
+          <TableRow>Make</TableRow>
+          <TableRow>Actions</TableRow>
+         </TableHead>
+         <TableBody>
+         { props.makes.map((make) => {
+          return <>
+          <TableRow key={make.MakeId}>
+           <TableCell>{make.MakeId}</TableCell>
+           <TableCell>{make.MakeName}</TableCell>
+           <TableCell></TableCell>
+          </TableRow>
+         </>
+         })}
+         </TableBody>
+        </Table>
+     </>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,23 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
+export const fetchMakes = () => {
+  return (dispatch) => {
+   fetch(url)
+   .then(response => response.json())
+   .then(data => {
+    const action = {
+    type: 'FETCH_MAKES',
+    value: data.Results
+    }
+    dispatch(action)
+   })
+   
+  }
+  
+  
+
+  
+
+}
